83f4a59506544be2c288ee3ad57d4349f8e2d14f
christian baidoo <chris_pens@hotmail.co.uk>
2020-06-15 21:24:39 +0100

    added error message to the console

--- vue.js
+++ vue.js
@@ -1,44 +1,45 @@
 const vueApp = new Vue({
-    el: "#app",
-    data: {
-        books: [],
-        search: "",
+  el: "#app",
+  data: {
+    books: [],
+    search: "",
+  },
+  methods: {
+    getData: function () {
+      fetch("https://api.myjson.com/bins/zyv02")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.books = data.books;
+          console.log(this.filteredBooks);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, //To show information when the search result equals to none.
+    filtered: function () {
+      let information = document.querySelector(".info");
+      console.log(this.filteredBooks);
+      if (this.filteredBooks.length <= 0) {
+        console.log(this.filteredBooks);
+        information.style.display = "block";
+      } else {
+        information.style.display = "none";
+      }
     },
-    methods: {
-        getData: function () {
-            fetch('https://api.myjson.com/bins/zyv02')
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    this.books = data.books;
-                    console.log(this.filteredBooks)
-                });
-        }, //To show information when the search result equals to none.
-        filtered: function () {
-            let information = document.querySelector(".info")
-            console.log(this.filteredBooks)
-            if (this.filteredBooks.length <= 0) {
-                console.log(this.filteredBooks)
-                information.style.display = "block"
-            } else {
-                information.style.display = "none";
-            }
-        }
+  },
+  //Use the Created function to call the getData function
+  created() {
+    this.getData();
+  },
 
+  // function to filter the array based on user input in the search box
+  computed: {
+    filteredBooks: function () {
+      return this.books.filter((book) =>
+        book.title.toLowerCase().match(this.search.toLowerCase())
+      );
     },
-    //Use the Created function to call the getData function
-    created() {
-        this.getData();
-    },
-
-    // function to filter the array based on user input in the search box
-    computed: {
-        filteredBooks: function () {
-            return this.books.filter((book) => book.title.toLowerCase().match(this.search.toLowerCase()));
-        },
-    },
-
-
-
-})
+  },
+});
